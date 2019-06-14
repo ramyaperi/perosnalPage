062e14a976e7a25c323a5129ad84ef10574037da
ramyaperi <ramyaperi2@gmail.com>
2019-06-15 00:18:44 +0200

    'lexbot'

--- src/LexBot.js
+++ src/LexBot.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Button } from "react";
 import Amplify, { Interactions } from "aws-amplify";
 import { ChatBot, AmplifyTheme } from "aws-amplify-react";
 import awsmobile from "./aws-exports";
@@ -11,6 +11,7 @@ class LexBot extends Component {
   constructor(props) {
     super(props);
     this.handleChat = this.handleChat.bind(this);
+    this.closeChat = this.closeChat.bind(this);
   }
   state = {
     visibility: false
@@ -25,7 +26,13 @@ class LexBot extends Component {
   }
   handleChat() {
     this.setState(state => ({
-      visibility: !this.state.visibility
+      visibility: true
+    }));
+    Interactions.send("Resume_webpage", "Download resume");
+  }
+  closeChat() {
+    this.setState(state => ({
+      visibility: false
     }));
   }
   handleComplete(err, confirmation) {
@@ -34,29 +41,36 @@ class LexBot extends Component {
       return;
     }
     //alert("Success: " + JSON.stringify(confirmation, null, 2));
+
     return "Here is the resume. Thank you!";
   }
   render() {
-    if (this.state.visibility) {
-      return (
-        <div className="chatWindow">
-          <ChatBot
-            theme={MyTheme}
-            title="My React Bot"
-            botName="Resume_webpage"
-            welcomeMessage="Hello, Which langauage would you like to download the resume in English/Deutsch?"
-            onComplete={this.handleComplete.bind(this)}
-            clearOnComplete={true}
-          />
-        </div>
-      );
-    } else {
-      return (
-        <a href="#" className="float" onClick={this.handleChat}>
-          <i className="fa fas fa-comments my-float" />
-        </a>
-      );
-    }
+    const visibility = this.state.visibility;
+    return (
+      <div>
+        {visibility ? (
+          <div className="chatWindow">
+            {/*Do not want to customise chatbot only to add close button so using
+          anchor to look like a close button */}
+            <a class="chatClose" id="chatClose" onClick={this.closeChat}>
+              x
+            </a>
+            <ChatBot
+              theme={MyTheme}
+              title="My React Bot"
+              botName="Resume_webpage"
+              welcomeMessage="Hello, Which langauage would you like to download the resume in English/Deutsch?"
+              onComplete={this.handleComplete.bind(this)}
+              clearOnComplete={true}
+            />
+          </div>
+        ) : (
+          <a href="#" className="float" onClick={this.handleChat}>
+            <i className="fa fas fa-comments my-float" />
+          </a>
+        )}
+      </div>
+    );
   }
 }
 
